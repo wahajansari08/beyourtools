01e9c5fd785581de684af3615b76d694f3593f9e
wahajansari08 <wahajansari08@gmail.com>
2026-08-24 20:19:46 +0500

    fixed redirect issues

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: process.cwd(),
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.beyourtools.com" }],
+        destination: "https://beyourtools.com/:path*",
+        permanent: true,
+      },
+    ];
   },
 };
 
