5a253702615b2917d0f75decaf3fcbea8f2d8c29
wahajansari08 <wahajansari08@gmail.com>
2026-09-18 02:05:16 +0500

    http homepage security fixed

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/:path*",
+        has: [{ type: "header", key: "x-forwarded-proto", value: "http" }],
+        destination: "https://beyourtools.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
         has: [{ type: "host", value: "www.beyourtools.com" }],
         destination: "https://beyourtools.com/:path*",
         permanent: true,
